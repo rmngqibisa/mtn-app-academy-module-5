0c1673c6f8e0b1cf87ebd9de0080c302fbd01478
Romeo Mngqibisa <romeomngqibisa@gmail.com>
2022-09-12 23:05:35 +0200

    Updated UIX

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-import { NavigationContainer, StackActions,  } from '@react-navigation/native';
+import { Link, NavigationContainer, StackActions,  } from '@react-navigation/native';
 import { setStatusBarHidden, StatusBar } from 'expo-status-bar';
-import { Button, Image, StyleSheet, Text, TextInput, View, Appearance, Share } from 'react-native';
+import { Button, Image, StyleSheet, Text, TextInput, View, Appearance, Share, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
 import TaxBro_pink from './assets/TaxBro_pink.png';
 import logo from './assets/logo.png';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -25,26 +24,39 @@ function App() {
   
 
 function LoginScreen({ navigation }) {
-  return (<View style={styles.container}>
-    <Text style={{ color: "#fff", fontSize: 25, fontWeight: 'bold', }}>Login</Text>
+  return (
+  <KeyboardAvoidingView style={styles.container} behavior='padding'>
+    <Text style={{ color: "#fff", fontSize: 25, fontWeight: 'bold', styles}}>Login</Text>
     <Text style={{ Maxheight:90, height:70 }}></Text>
-    <Image source={TaxBro_pink} style={{ width: 250, height: 250 }} />
-    <Text style={{ color: "#fff", fontSize: 15,  }}>Email Address</Text>
-    <TextInput style={{ width: 200, backgroundColor: "#fff", color: 'black', height: 30 }}
+    <Image source={TaxBro_pink} style={{ width: 250, height: 220 }} />
+    <View style={styles.inputContainer}>
+    <TextInput style={styles.input}
       editable
-      maxLength={ 40 }
-      minLength={ 5 } 
+      value=''
+      placeholder='Email Address'
+      onChangeText={text => ''}
       />
     <Text></Text>
-    <Text style={{ color: "#fff", fontSize: 15 }}>Password</Text>
-    <TextInput style={{ width: 200, backgroundColor: "#fff", color: 'black', height: 30 }}
+      <TextInput style={styles.input}
       editable
+      placeholder='Password'
       maxLength={ 40 } 
       secureTextEntry={true}
-      onChangeText=""
+      onChangeText={text => ""}
       />
+    </View>
+
     <Text></Text>
-    <Button title='Sign In' onPress={ () => { navigation.navigate("Dashboard"); }}></Button>
+    
+    <View style={styles.buttonContainer}>
+    <TouchableOpacity
+      onPress={() => {navigation.navigate("Dashboard"); }} 
+      style={[styles.button, styles.buttonOutline]}
+      >
+      <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+    </View>
+    
     <Text></Text>
     <Text style={{ color: "#FFF", fontSize: 14, }}>Forgot your Password?</Text>
     <Text onPress={() => (navigation.navigate("Password Reset"))} 
@@ -54,75 +66,102 @@ function LoginScreen({ navigation }) {
     <Text style={{ color: "#fff", fontSize: 14, position:"absolute", bottom:35 }}>Don't have an account? </Text>
     <Text onPress={() => navigation.navigate("Sign Up")}
            style={{ fontSize: 14, color: '#00F', position:"absolute", bottom:15 }} >Click here to Sign Up.</Text>
-  </View>
+  </KeyboardAvoidingView>
   );
 }
 
 function SignUpScreen({ navigation }) {
   return(
-    <View style={styles.container}>
-    <Image source={TaxBro_pink} style={{ width: 150, height: 250}} />
-    <Text style={{ color: "#fff", fontSize: 15, Maxheight: 30.50 }}>First Name</Text>
-    <TextInput style={{ width: 200, backgroundColor: "#fff", color: 'black', height: 30 }}
+    <KeyboardAvoidingView style={styles.container}>
+    <Image source={TaxBro_pink} style={{ width: 150, height: 150}} />
+  
+    <View style={styles.inputContainer}>
+    <TextInput style={styles.input}
       editable
-      maxLength={ 40 }
-      minLength={ 5 } 
-      onChangeText=""
+      value=''
+      placeholder='First Name'
+      onChangeText={text => ''}
       />
       <Text></Text>
-      <Text style={{ color: "#fff", fontSize: 15, Maxheight: 30.50 }}>Last Name</Text>
-      <TextInput style={{ width: 200, backgroundColor: "#fff", color: 'black', height: 30 }}
+      <TextInput style={styles.input}
       editable
-      maxLength={ 40 }
-      minLength={ 5 } 
-      onChangeText=""
+      value=''
+      placeholder='Last Name'
+      onChangeText={text => ''}
       />
       <Text></Text>
-      <Text style={{ color: "#fff", fontSize: 15, Maxheight: 30.50 }}>Email Address</Text>
-      <TextInput style={{ width: 200, backgroundColor: "#fff", color: 'black', height: 30 }}
+      <TextInput style={styles.input}
       editable
-      maxLength={ 40 }
-      minLength={ 5 } 
-      onChangeText=""
+      value=''
+      placeholder='Email Address'
+      onChangeText={text => ''}
       />
       <Text></Text>
-      <Text style={{ color: "#fff", fontSize: 15, Maxheight: 30.50 }}>Password</Text>
-      <TextInput style={{ width: 200, backgroundColor: "#fff", color: 'black', height: 30,  }}
+      <TextInput style={styles.input}
       editable
-      maxLength={ 40 }
-      minLength={ 5 } 
-      onChangeText=""
+      value=''
+      placeholder='Password'
+      onChangeText={text => ''}
       />
+      <Text></Text>
+      <TextInput style={styles.input}
+      editable
+      value=''
+      placeholder='Enter The Same Password'
+      onChangeText={text => ''}
+      />
+      </View>
+
     <Text></Text>
-    <Button title='Sign Up' onPress={() => { navigation.navigate("Login"); }} style={{ top: 0  }}></Button>  
+
+  <View style={styles.buttonContainer}>
+    <TouchableOpacity
+      onPress={() => {navigation.navigate("Login"); }} 
+      style={[styles.button, styles.buttonOutline]}
+      >
+      <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
+    </View>
+    
     <Text style={{ height:90, Maxheight:100 }}></Text>
     <Text style={{ color: "#fff", fontSize: 14, position:"absolute", bottom:35 }}>Already have an account? </Text>
     <Text onPress={() => navigation.navigate("Login")}
            style={{ fontSize: 14, color: '#00F', position:"absolute", bottom:15 }} >Click here to Sign In.</Text>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
 function PasswordResetScreen({ navigation }) {
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView style={styles.container}>
       <Text style={{ color: "#fff", fontSize: 25, fontWeight: 'bold', }}></Text>
-      <Text style={{ Maxheight:90, height:70 }}></Text>
+      <Text style={{ Maxheight:50, height:40 }}></Text>
       <Image source={TaxBro_pink} style={{ width: 250, height: 200 }} />
       <Text style={{ color: "#fff", fontSize: 15, textAlign: "center", fontWeight: "bold"}} >Please enter your email address to reset your Password.</Text>
-      <Text style={{ height:35 }}></Text>
-      <Text style={{ color: "#fff", fontSize: 15, Maxheight: 30.50 }}>Email Address</Text>
-      <Text></Text>
-      <TextInput style={{ width: 200, backgroundColor: "#fff", color: 'black', height: 30, }}
+      <Text style={{ height:25 }}></Text>
+      
+      <View style={styles.inputContainer}>
+      <TextInput style={styles.input}
       editable
-      maxLength={ 40 }
-      minLength={ 5 } 
-      onChangeText=""
+      value=''
+      placeholder='Email Address'
+      onChangeText={text => ''}
       />
+      </View>
+
       <Text></Text>
-      <Button title='Submit' onPress={() => {navigation.navigate("Login")}}></Button>
+
+      <View style={styles.buttonContainer}>
+      <TouchableOpacity
+      onPress={() => {navigation.navigate("Login"); }} 
+      style={[styles.button, styles.buttonOutline]}
+      >
+      <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
+      </View>
+
       <Text style={{ height: 150 }}></Text>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -147,17 +186,22 @@ function DashboardScreen({ }) {
       }
     };
     return (
-      <View style={styles.container }>
+      <KeyboardAvoidingView style={styles.container }>
         <Image source={logo} style={{ height: 250, width: 250, position: 'relative', }}></Image>
         <Text></Text>
-        <Text style={{ color: "#000", fontSize: 15, textAlignVertical: 'center', textAlign: 'center' }}>Share the TaxBro 'Coming Soon' message to your friends, to tell their friends that Southa's Tax is about to be redefined and revolutionized - no affirmative repossessions necessary</Text>
+        <Text style={{ color: "#000", fontSize: 15, textAlignVertical: 'center', textAlign: 'center', margin: 17, fontWeight: '500' }}>Share the TaxBro 'Coming Soon' message to your friends, to tell their friends that Southa's Tax is about to be redefined and revolutionized - no affirmative repossessions necessary</Text>
+        
+        <View style={styles.buttonContainer}>
+        <TouchableOpacity
+           onPress={onShare} 
+           style={[styles.button, styles.buttonOutline]} >
+         <Text style={styles.buttonText}>Share</Text>
+         </TouchableOpacity>
+        </View>
+
+        <Text style={{ Maxheight:120, height: 90 }}></Text>
         <Text></Text>
-        <Text></Text>
-        <Text></Text>
-        <Button onPress={onShare} title="Share" />
-        <Text style={{ Maxheight:90, height: 180 }}></Text>
-        <Text></Text>
-      </View>
+      </KeyboardAvoidingView>
     );
   
 }
@@ -168,8 +212,48 @@ const styles = StyleSheet.create({
     backgroundColor: '#FF14A8',
     alignItems: 'center',
     justifyContent: 'center',
-  
   },
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10, 
+    marginTop: 5
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: "center",
+    alignItems: 'center',
+    marginTop: 20
+  },
+  button: {
+    backgroundColor: '#FF14A8',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center'
+  },
+  buttonOutline: {
+    backgroundColor: '#FF14A8',
+    marginTop: 5,
+    borderColor: '#fff',
+    borderWidth: 2
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: '700',
+    fontSize: 16
+  },
+  buttonOutlineText: {
+    color: '#FF14A8',
+    fontWeight: '700',
+    fontSize: 16
+  }
+
+
 });
 
 const options = StyleSheet.create({
